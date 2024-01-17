63db82abbe8282291077913264e22baa9a2d18ef
Martho Evan <martho.evan@gmail.com>
2024-01-17 11:28:31 +0100

    feat: add display and reset score

--- clicker/ContentView.swift
+++ clicker/ContentView.swift
@@ -7,17 +7,30 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var score = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Score: \(score)")
+                .font(.title)
+                .padding()
+            Image(systemName: "plus.square")
+                .font(.title)
+                .onTapGesture {
+                    score = score+1
+                }
+            Spacer()
+            Button("New game"){
+                score = 0
+            }.padding()
         }
-        .padding()
     }
 }
+
+//func addingScore(score) -> <#return type#> {
+//    <#function body#>
+//}
 
 #Preview {
     ContentView()
