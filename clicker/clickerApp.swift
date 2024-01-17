1ccc11d871e1916cebe2eff10bc4d8204eb15cbd
Martho Evan <martho.evan@gmail.com>
2024-01-17 10:41:48 +0100

    Initial Commit

---
+++ clicker/clickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  clickerApp.swift
+//  clicker
+//
+//  Created by Evan Martho on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct clickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
