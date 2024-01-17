217f00a1486d72566e74579e211bf416e6b7a56b
Martho Evan <martho.evan@gmail.com>
2024-01-17 11:55:30 +0100

    feat: manage the best score

--- clicker/ContentView.swift
+++ clicker/ContentView.swift
@@ -12,12 +12,18 @@ struct ContentView: View {
     
     @State var score = 0
     @State var gameIsInProgress = false
+    @State var bestScore = 0
     
     var body: some View {
         VStack {
-            Text("Score: \(score)")
-                .font(.title)
+            HStack {
+                if score >= bestScore && score != 0 {
+                    Image(systemName: "flame")
+                }
+                Text("Score: \(score)")
                 .padding()
+            }.font(.title)
+            
             if gameIsInProgress == true {
                 Image(systemName: "plus.square")
                     .font(.title)
@@ -26,12 +32,16 @@ struct ContentView: View {
                     }
             }
             Spacer()
+            
             if gameIsInProgress == false {
                 Button("New game"){
                     score = 0
                     gameIsInProgress = true
                     Timer.scheduledTimer(withTimeInterval: 10, repeats: false) { (_) in
                         gameIsInProgress = false
+                        if score > bestScore {
+                            bestScore = score
+                        }
                     }
                 }.padding()
             }
