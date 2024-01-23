30248be5f91c8b76fe0b3e71aa73f1c6d9584de4
Martho Evan <martho.evan@gmail.com>
2024-01-23 16:58:16 +0100

    feat: manage the username

--- clicker/ContentView.swift
+++ clicker/ContentView.swift
@@ -13,15 +13,36 @@ struct ContentView: View {
     @State var score = 0
     @State var gameIsInProgress = false
     @State var bestScore = 0
+    @State var newGame = false
+    @State var userName:String = ""
+    @AppStorage("userNameStorage") var userNameStorage: String = ""
     
     var body: some View {
         VStack {
+            // Manage text field and the button on the right
+            HStack{
+                TextField("Please enter user name",text: $userName)
+                if(userName == ""){
+                    Image(systemName: "plus.circle")
+                        .padding()
+                        .onSubmit {
+                            userNameStorage = userName
+                        }
+                }else{
+                    Image(systemName: "minus.circle")
+                        .padding()
+                        .onTapGesture {
+                            userName = ""
+                        }
+                }
+            }.padding()
+            //Manage the best score system
             HStack {
                 if score >= bestScore && score != 0 {
-                    Image(systemName: "flame")
+                    Image(systemName: "flame.fill")
                 }
                 Text("Score: \(score)")
-                .padding()
+                    .padding()
             }.font(.title)
             
             if gameIsInProgress == true {
@@ -32,7 +53,17 @@ struct ContentView: View {
                     }
             }
             Spacer()
-            
+            //Display the username of best score
+            if(bestScore != 0){
+                HStack{
+                    Image(systemName: "plus.square")
+                    Text("\(userName) - \(bestScore)")
+                    Image(systemName: "plus.square")
+                    
+                }
+            }
+            Spacer()
+            //Manage the new game
             if gameIsInProgress == false {
                 Button("New game"){
                     score = 0
@@ -45,6 +76,7 @@ struct ContentView: View {
                     }
                 }.padding()
             }
+            
         }
     }
 }
